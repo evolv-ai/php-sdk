efd663224ca9d1a25a241fa79132eb6ce31db884
sonik <yagoda303@gmail.com>
2022-06-10 13:08:09 +0200

    added for composer.json, for package library packagist

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+declare (strict_types=1);
+
+use Sdk\EvolvSdk\Test;
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+?>
---
+++ src/test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare (strict_types=1);
+
+namespace Sdk\EvolvSdk;
+
+class Context
+{
+
+    public function __construct()
+
+    {
+
+        echo "Hello Wold";
+
+    }
+
+}
+
+?>
