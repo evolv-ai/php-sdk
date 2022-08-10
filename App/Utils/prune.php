01678d27cf6e4de9a48f8c9a828c72e532dd42c9
Sonik <sonik@redwerk.com>
2022-08-10 21:56:51 +0300

    updated prune  function

--- App/Utils/prune.php
+++ App/Utils/prune.php
@@ -23,8 +23,52 @@ function prune(array $array, array $active)
         }
     }
 
-    // TODO: handle predicated values
-    // reattributePredicatedValues($pruned, $active);
+     reattributePredicatedValues($pruned, $active);
 
     return $pruned;
 }
+
+/**
+ * @param array $pruned
+ * @param array $active
+ * @returns void
+ */
+function reattributePredicatedValues($pruned, $active) {
+
+    reattribute( [], $active, []);
+
+    reattribute($pruned, $active, []);
+}
+
+function reattribute( array $obj, array $active, $collected = null) {
+
+    if (!is_array($obj) || empty($obj)) {
+        return $obj;
+    }
+    if ($obj['_predicated_values']) {
+
+         $predicatedKeyPrefix = implode(".", $collected);
+
+        for ($i = 0; $i < count($obj['_predicated_values']); $i++) {
+
+            if ($predicatedKeyPrefix . '.' . $obj['_predicated_values'][$i]['_predicate_assignment_id'] && in_array( $predicatedKeyPrefix,$active)) {
+                return $obj['_predicated_values'][$i]['_value'];
+            }
+        }
+
+        return null;
+    }
+    $keys = array_keys($obj);
+
+    for ( $i = 0; $i < count($keys); $i++) {
+        $key = $keys[$i];
+        $newCollected = array_merge($collected,[$key]);
+        if(is_array($obj[$key])) {
+            $obj[$key] = reattribute($obj[$key], $active, $newCollected);
+        }
+    }
+
+    return $obj;
+}
+
+
